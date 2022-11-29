3fe1028bfe28c964cd0d6fa0f1b86fd32b7e93df
David Cervantes <davidcer@live.unc.edu>
2022-11-29 01:55:46 -0500

    first try

---
+++ server.js
@@ -0,0 +1,56 @@
+import { roll } from './lib/roll.js'
+import express from 'express'
+const app = express()
+
+const port = 5000
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
+
+
+app.get('/app/', (req, res, next) => {
+	res.send('200 OK')
+})
+app.get('/app/roll/', (req, res, next) => {
+	const sides = req.query.sides || 6
+	const dice = req.query.dice || 2
+	const rolls = req.query.rolls || 1
+	res.json(roll(sides, dice, rolls))
+})
+app.get('/app/roll/:sides/', (req, res, next) => {
+	const sides = req.params.sides
+	const dice = req.query.dice || 2
+	const rolls = req.query.rolls || 1
+	res.json(roll(sides, dice, rolls))
+})
+app.get('/app/roll/:sides/:dice', (req, res, next) => {
+	const sides = req.params.sides
+	const dice = req.parms.dice
+	const rolls = req.query.rolls || 1
+	res.json(roll(sides, dice, rolls))
+})
+app.get('/app/roll/:sides/:dice/:rolls', (req, res, next) => {
+	const sides = req.params.sides
+	const dice = req.params.dice
+	const rolls = req.params.rolls
+	res.json(roll(sides, dice, rolls))
+})
+app.get('*', (req, res, next) => {
+	res.status(404).send('404 NOT FOUND')
+})
+
+
+
+app.post('/app/roll', (req, res, next) => {
+	const sides = req.body.sides || 6
+	const dice = req.body.dice || 2
+	const rolls = req.body.rolls || 1
+	res.json(roll(sides, dice, rolls))
+})
+app.post('*', (req, res, next) => {
+	res.status(404).send('404 NOT FOUND')
+})
+
+app.listen(port, () => {
+	console.log('Server listening on port ' + port);
+})
